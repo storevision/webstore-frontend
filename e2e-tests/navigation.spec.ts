92f696ff0f0bb2ced184cf4afe6fa62e04e72eb2
CommanderRedYT <ccommanderred@gmail.com>
2024-11-20 11:41:17 +0100

    Improve tests

---
+++ e2e-tests/navigation.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test';
+
+test('should navigate to the about page', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    // find element with the test-id "nav-about"
+    const aboutButton = page.getByTestId('nav-about');
+
+    await aboutButton.click();
+
+    // assert the URL is correct
+    await expect(page).toHaveURL('http://localhost:3000/about');
+
+    // assert the page title is correct
+    await expect(page.title()).resolves.toContain('About');
+});
+
+test('should navigate to the home page', async ({ page }) => {
+    await page.goto('http://localhost:3000/about');
+
+    // find element with the test-id "nav-home"
+    const homeButton = page.getByTestId('nav-home');
+
+    await homeButton.click();
+
+    // assert the URL is correct
+    await expect(page).toHaveURL('http://localhost:3000/');
+
+    // assert the page title is correct
+    await expect(page.title()).resolves.toContain('Home');
+});
