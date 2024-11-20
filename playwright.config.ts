4de39255ef4407e31b2da23dcf98e24c4524c51b
CommanderRedYT <ccommanderred@gmail.com>
2024-11-20 11:20:43 +0100

    Start dev server for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,10 +70,10 @@ export default defineConfig({
         // },
     ],
 
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    /* Run nextjs dev server before starting the tests (only for CI) */
+    webServer: {
+        command: 'yarn dev',
+        port: 3000,
+        reuseExistingServer: !process.env.CI,
+    },
 });
