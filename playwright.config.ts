31dabeb7c20c43dfa78c37a6c523218940c54296
CommanderRedYT <ccommanderred@gmail.com>
2024-12-10 07:48:54 +0100

    Also report json

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,9 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     // reporter: process.env.CI ? 'github' : 'html',
-    reporter: process.env.CI ? [['github'], ['html']] : 'html',
+    reporter: process.env.CI
+        ? [['github'], ['html'], ['json', { outputFile: 'results.json' }]]
+        : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
